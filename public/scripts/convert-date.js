384c27508b1d5f87e5e0ab212c55715f82c18e9b
Scott Veale <srveale42@gmail.com>
2017-01-27 20:35:07 +0000

    allowed for non-plural time units

--- public/scripts/convert-date.js
+++ public/scripts/convert-date.js
@@ -4,24 +4,24 @@ const convertDate = function (date) {
   if (date > 86400) {
     var days = Math.floor(date / 86400);
     date -= days * 86400;
-    return days + " days ago";
+    return days > 1 ? days + " days ago": days + " day ago";
   }
 
   // calculate (and subtract) whole hours
   if (date > 3600) {
     var hours = Math.floor(date / 3600) % 24;
     date -= hours * 3600;
-    return hours + " hours ago";
+    return hours > 1 ? hours + " hours ago" : hours + " hour ago";
   }
 
   // calculate (and subtract) whole minutes
   if (date > 60) {
     var minutes = Math.floor(date / 60) % 60;
     date -= minutes * 60;
-    return minutes + " minutes ago";
+    return minutes > 1 ? minutes + " minutes ago" : minutes + " minute ago";
   }
 
   // what's left is seconds
   var seconds = Math.floor(date % 60);  // in theory the modulus is not required
-  return seconds + " seconds ago";
+  return seconds > 1 ? seconds + " seconds ago": seconds + " second ago";
 }
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient   = require("mongodb").MongoClient;
-const MONGODB_URI   = "mongodb://localhost:27017/testTweeter3";
+const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
